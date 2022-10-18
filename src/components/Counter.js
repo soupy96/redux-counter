c60c2ae256f1531d73a800a7309561bc35d36b41
Michael Campbell <michael.campbell14@gmail.com>
2022-10-18 17:51:06 -0400

    using useDispatch to dispatch actions to our redux store and attaching them to the buttons. so far this is a lot easier to use, understand and follow the context

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,7 +1,8 @@
 // we can import useSelector which allows us to select a specific point in the store
 // using useStore gives us the whole store
 // if we're using class based components we can use "connect" which is used as a wrapper around the class component to connect it to the store
-import { useSelector } from 'react-redux';
+// use useDispatch to dispatch actions to our store
+import { useSelector, useDispatch } from 'react-redux';
 
 import classes from './Counter.module.css';
 
@@ -12,12 +13,27 @@ const Counter = () => {
   // if we remove this component from the app react-redux would automatically remove the subscription for me
   const counter = useSelector((state) => state.counter);
 
+  // gives us a dispatch function that we can call against our redux store
+  const dispatch = useDispatch();
+
+  const incrementHandler = () => {
+    dispatch({ type: 'increment' });
+  };
+
+  const decrementHandler = () => {
+    dispatch({ type: 'decrement' });
+  };
+
   const toggleCounterHandler = () => {};
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
       <div className={classes.value}>{counter}</div>
+      <div>
+        <button onClick={incrementHandler}>Increment</button>
+        <button onClick={decrementHandler}>Decrement</button>
+      </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
