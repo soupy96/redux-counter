e5a81dda2ef53386612b5623d9075d333cb3d71a
Michael Campbell <michael.campbell14@gmail.com>
2022-10-18 17:35:16 -0400

    providing the store to the whole app

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
 
 import './index.css';
 import App from './App';
+import store from './store/index';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(
+  // this provides the store to the whole app at the most top level
+  // with this we can access store in each component and child component
+  // they can become a subscriber to the store and dispatch actions to the store
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
