83f14bd35ae18c6f61a36142132bbd57c2a77ccb
Michael Campbell <michael.campbell14@gmail.com>
2022-10-20 16:30:57 -0400

    first using createSlice the the reduxjs toolkit

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,41 @@
 import { createStore } from 'redux';
+import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = { counter: 0, showCounter: true };
 
+// createSlice() needs an object
+// creating a slice of our global state that are similar, you can make more than one slice for different things
+// every slice needs a name, a specific identifier
+// needs an intial state
+// add reducers, an object/map, of all the reducers that this state slice needs
+// each method needs to take in the state
+// in the object we are allowed to mutate the state bccause with redux toolkit we cant accidentally manipulate the existing state
+// as well as recieving the state the methods can recieve the action payload. its not required but optional
+createSlice({
+  name: 'counter',
+  initialState,
+  reducers: {
+    increment(state) {
+      state.counter++;
+    },
+    decrement(state) {
+      state.counter--;
+    },
+    increase(state, action) {
+      state.counter = state.counter + action.amount;
+    },
+    toggleCounter(state) {
+      state.showCounter = !state.showCounter;
+    },
+  },
+});
+
 const counterReducer = (state = initialState, action) => {
   if (action.type === 'increment') {
+    // you need to return everything in the state cause redux doesnt merge everything in the existing state it replaces it
+    // never change/mutate the OG state
+    // always copy and create new objects
     return {
-      // you need to return everything in the state cause redux doesnt merge everything in the existing state it replaces it
       counter: state.counter + 1,
       showCounter: state.showCounter,
     };
